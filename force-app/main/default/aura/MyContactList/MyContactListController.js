b26b9aec57600dbca666363b4c5a713cdd9ab270
Adam-Southgate <dome3@hotmail.co.uk>
2018-10-12 15:09:14 -0400

    Added client side controller

--- force-app/main/default/aura/MyContactList/MyContactListController.js
+++ force-app/main/default/aura/MyContactList/MyContactListController.js
@@ -1,5 +1,11 @@
 ({
     myAction : function(component, event, helper) {
-
-    }
+        var action = component.get("c.getContacts");
+        action.setParams({
+            recordId: component.get("v.recordId")
+        });
+        action.setCallback(this, function(data) {
+            component.set("v.Contacts", data.getReturnValue());
+        });
+        $A.enqueueAction(action);}
 })
